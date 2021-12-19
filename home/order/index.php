d5c5cd9bfb69206e5833afe23a579168be33d54a
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2021-12-19 14:23:11 +0100

    Approve Functional

--- home/order/index.php
+++ home/order/index.php
@@ -10,5 +10,21 @@ $sql = " SELECT * FROM `order` ";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+if (isset($_POST['approved'])){
+  $approved = checkData($_POST['app']);
+  $id = checkData($_POST['id']);
+  
+  // echo $approved.$id;
+  // echo $approved.$id;
+  $approved=$approved?0:1;
+
+  $sql = " UPDATE `order` SET `approved` = :approved WHERE `ID` = :id; ";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([
+    'id' => $id,
+    'approved' =>$approved
+  ]);   
+  header("Refresh:0");
+}
 include '../layout.phtml';
 ?>
